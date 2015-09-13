04773dcbb48376c7d1c1d59646763f5865f1136c
madouh <tin.tin.nagi@gmail.com>
2015-09-13 14:20:49 +0200

    last commit

--- js/jone.js
+++ js/jone.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-//var node = document.getElementById("vall");
-//var x = false;
-//n = setInterval(function(){
-//if (x == false)
-//node.style.color="pink";
-//else
-//node.style.color="red";
-//x = !x;
-//},300);
+// var node = document.getElementById("vall");
+// var x = false;
+// n = setInterval(function(){
+// if (x == false)
+// node.style.color="pink";
+// else
+// node.style.color="red";
+// x = !x;
+// },300);
 // ==================================================
 var speed = 800;
 $("#top_message").slideDown(speed);
@@ -16,65 +16,65 @@ $("#top_message").slideUp(speed);
 
 });
 // =================================================
-var nodes = document.getElementById("lll");
-var z = false;
-n = setInterval(function(){
-if (z == false)
-nodes.style.color="green";
-else
-nodes.style.color="orange";
-z = !z;
-},300);
+// var nodes = document.getElementById("lll");
+// var z = false;
+// n = setInterval(function(){
+// if (z == false)
+// nodes.style.color="green";
+// else
+// nodes.style.color="orange";
+// z = !z;
+// },300);
 // ====================================================
-var nods = document.getElementById("ggg");
-var a = false;
-n = setInterval(function(){
-if (a == false)
-nods.style.color="purple";
-else
-nods.style.color="red";
-a = !a;
-},300);
+// var nods = document.getElementById("ggg");
+// var a = false;
+// n = setInterval(function(){
+// if (a == false)
+// nods.style.color="purple";
+// else
+// nods.style.color="red";
+// a = !a;
+// },300);
 // =================================================
-var nds = document.getElementById("nnk");
-var x = false;
-n = setInterval(function(){
-if (x == false)
-nds.style.color="red";
-else
-nds.style.color="pink";
-x = !x;
-},300);
+// var nds = document.getElementById("nnk");
+// var x = false;
+// n = setInterval(function(){
+// if (x == false)
+// nds.style.color="red";
+// else
+// nds.style.color="pink";
+// x = !x;
+// },300);
 // ===============================================
-var nos = document.getElementById("uou");
-var y = false;
-n = setInterval(function(){
-if (y == false)
-nos.style.color="red";
-else
-nos.style.color="pink";
-y = !y;
-},300);
+// var nos = document.getElementById("uou");
+// var y = false;
+// n = setInterval(function(){
+// if (y == false)
+// nos.style.color="red";
+// else
+// nos.style.color="pink";
+// y = !y;
+// },300);
 // ===============================================
-var ns = document.getElementById("ddd");
-var w = false;
-n = setInterval(function(){
-if (w == false)
-ns.style.color="lime";
-else
-ns.style.color="green";
-w = !w;
-},300);
+// var ns = document.getElementById("ddd");
+// var w = false;
+// n = setInterval(function(){
+// if (w == false)
+// ns.style.color="lime";
+// else
+// ns.style.color="green";
+// w = !w;
+// },300);
 // ================================================
-var os = document.getElementById("uou");
-var l = false;
-n = setInterval(function(){
-if (l == false)
-os.style.color="red";
-else
-os.style.color="pink";
-l = !l;
-},900);
+// var os = document.getElementById("uou");
+// var l = false;
+// n = setInterval(function(){
+// if (l == false)
+// os.style.color="red";
+// else
+// os.style.color="pink";
+// l = !l;
+// },900);
 
 
 
